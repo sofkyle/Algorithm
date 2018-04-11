effa17ffae0c667e211e6c99fa1a33fb6a5b40d2
glorychou <fightzyf1992@hotmail.com>
2018-04-12 04:11:27 +0800

    SearchInsert

---
+++ LeetCodePrj/Java/leetcode/Easy.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author Kyle
+ * @create 2018/4/12 2:45
+ */
+public class Easy {
+
+    @Test
+    public void twoSum() {
+         TwoSum twoSum =  new TwoSum();
+         int nums[] = {2, 7, 11, 15};
+         int target = 9;
+         System.out.println(Arrays.toString(twoSum.twoSum(nums, target)));
+    }
+
+    @Test
+    public void searchInsert() {
+        SearchInsert searchInsert = new SearchInsert();
+        int nums[] = {1, 3, 5, 6};
+        int target = 0;
+        System.out.println(searchInsert.searchInsert(nums, target));
+    }
+}
