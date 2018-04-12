0b6f14e6a861fecf3711e6839ce0ab063c63babf
glorychou <fightzyf1992@hotmail.com>
2018-04-13 03:01:16 +0800

    CountAndSay

--- LeetCodePrj/Java/leetcode/easy/Easy.java
+++ LeetCodePrj/Java/leetcode/easy/Easy.java
@@ -1,4 +1,4 @@
-package leetcode;
+package leetcode.easy;
 
 import org.junit.jupiter.api.Test;
 
@@ -24,5 +24,12 @@ public class Easy {
         int nums[] = {1, 3, 5, 6};
         int target = 0;
         System.out.println(searchInsert.searchInsert(nums, target));
+    }
+
+    @Test
+    public void countAndSay() {
+        CountAndSay countAndSay = new CountAndSay();
+        int n = 3;
+        System.out.println(countAndSay.countAndSay(n));
     }
 }
