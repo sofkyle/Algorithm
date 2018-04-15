82ed51b53b23f6da7a13e0b5c710192513bd6b9f
glorychou <fightzyf1992@hotmail.com>
2018-04-15 23:04:13 +0800

    sqrt

---
+++ LeetCodePrj/Java/leetcode/easy/SqrtX.java
@@ -0,0 +1,23 @@
+package leetcode.easy;
+
+/**
+ * @author Kyle
+ * @create 2018/4/15 1:49
+ */
+public class SqrtX {
+    public int sqrt(int x) {
+        if (x == 0)
+            return 0;
+        int left = 1, right = x;
+        while (true) {
+            int mid = left + (right - left)/2;
+            if (mid > x/mid) {
+                right = mid - 1;
+            } else {
+                if (mid + 1 > x/(mid + 1))
+                    return mid;
+                left = mid + 1;
+            }
+        }
+    }
+}
