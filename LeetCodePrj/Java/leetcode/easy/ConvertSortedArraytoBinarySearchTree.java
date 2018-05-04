bb33d6f3db2116fc039d0846a4787a839316c1ec
glorychou <fightzyf1992@hotmail.com>
2018-05-04 23:08:28 +0800

    Convert Sorted Array to Binary Search Tree

---
+++ LeetCodePrj/Java/leetcode/easy/ConvertSortedArraytoBinarySearchTree.java
@@ -0,0 +1,18 @@
+package leetcode.easy;
+
+/**
+ * @author Kyle
+ * @create 2018/5/4 23:02
+ */
+public class ConvertSortedArraytoBinarySearchTree {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return null;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
