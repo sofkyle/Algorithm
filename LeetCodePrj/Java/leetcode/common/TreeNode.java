a189b3078b22081c858cd46101a0b14c3846e571
glorychou <fightzyf1992@hotmail.com>
2018-07-22 23:22:43 +0800

    add common data structure

---
+++ LeetCodePrj/Java/leetcode/common/TreeNode.java
@@ -0,0 +1,12 @@
+package leetcode.common;
+
+/**
+ * @author Kyle
+ * @create 2018/7/22 23:20
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
