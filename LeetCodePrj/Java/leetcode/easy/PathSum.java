f3668d61361a1713da75683e66df9cd2ba5b5714
glorychou <fightzyf1992@hotmail.com>
2018-07-22 23:24:08 +0800

    Path Sum

---
+++ LeetCodePrj/Java/leetcode/easy/PathSum.java
@@ -0,0 +1,13 @@
+package leetcode.easy;
+
+import leetcode.common.TreeNode;
+
+/**
+ * @author Kyle
+ * @create 2018/7/22 23:23
+ */
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        return false;
+    }
+}
