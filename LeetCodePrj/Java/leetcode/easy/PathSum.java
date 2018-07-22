0a907517466bf101069abbbf3ea6d6af458b8ab6
glorychou <fightzyf1992@hotmail.com>
2018-07-22 23:49:14 +0800

    submit version

--- LeetCodePrj/Java/leetcode/easy/PathSum.java
+++ LeetCodePrj/Java/leetcode/easy/PathSum.java
@@ -1,13 +1,36 @@
 package leetcode.easy;
 
-import leetcode.common.TreeNode;
 
 /**
  * @author Kyle
  * @create 2018/7/22 23:23
  */
 public class PathSum {
+
+    private int actualSum = 0;
+
     public boolean hasPathSum(TreeNode root, int sum) {
-        return false;
+        if(root == null) {
+            return false;
+        }
+
+        if(root.left == null && root.right == null) {
+            actualSum += root.val;
+            if(actualSum == sum) return true;
+            else return false;
+        } else if(root.left == null) {
+            return hasPathSum(root.right, sum);
+        } else if(root.right == null) {
+            return hasPathSum(root.left, sum);
+        } else {
+            return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+        }
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
     }
 }
