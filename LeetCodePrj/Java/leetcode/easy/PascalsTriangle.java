0ffcca6ca24fe389c003e7e6da254a22ba8119c8
glorychou <fightzyf1992@hotmail.com>
2018-07-24 23:28:25 +0800

    first version

--- LeetCodePrj/Java/leetcode/easy/PascalsTriangle.java
+++ LeetCodePrj/Java/leetcode/easy/PascalsTriangle.java
@@ -1,5 +1,6 @@
 package leetcode.easy;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,6 +9,20 @@ import java.util.List;
  */
 public class PascalsTriangle {
     public List<List<Integer>> generate(int numRows) {
+        if(numRows == 1) {
+            return Arrays.asList(Arrays.asList(1));
+        }
+
+        int [][] pascalsTriangele = new int[numRows][numRows];
+        // init
+        pascalsTriangele[0][0] = 1;
+
+        for(int i = 1; i < numRows; i++) {
+            for(int j = 0; j <= i; j++) {
+                pascalsTriangele[i][j] = pascalsTriangele[i - 1][j] + pascalsTriangele[i - 1][i - 1];
+            }
+        }
+
         return null;
     }
 }
