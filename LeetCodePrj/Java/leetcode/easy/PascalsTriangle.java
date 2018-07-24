911ef9df52cd7197449b63869c69e1a9995b82df
glorychou <fightzyf1992@hotmail.com>
2018-07-24 23:47:33 +0800

    AE

--- LeetCodePrj/Java/leetcode/easy/PascalsTriangle.java
+++ LeetCodePrj/Java/leetcode/easy/PascalsTriangle.java
@@ -1,6 +1,8 @@
 package leetcode.easy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -9,20 +11,30 @@ import java.util.List;
  */
 public class PascalsTriangle {
     public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> pascalsTriangele = new ArrayList<>();
+
         if(numRows == 1) {
             return Arrays.asList(Arrays.asList(1));
         }
 
-        int [][] pascalsTriangele = new int[numRows][numRows];
+        int [][] pascalsTriangeleArray = new int[numRows][numRows];
         // init
-        pascalsTriangele[0][0] = 1;
+        pascalsTriangeleArray[0][0] = 1;
 
         for(int i = 1; i < numRows; i++) {
             for(int j = 0; j <= i; j++) {
-                pascalsTriangele[i][j] = pascalsTriangele[i - 1][j] + pascalsTriangele[i - 1][i - 1];
+                pascalsTriangeleArray[i][j] = pascalsTriangeleArray[i - 1][j] + (j > 0 ? pascalsTriangeleArray[i - 1][i - 1] : 0);
             }
         }
 
-        return null;
+        for(int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for(int j = 0; j <= i; j++) {
+                row.add(pascalsTriangeleArray[i][j]);
+            }
+            pascalsTriangele.add(row);
+        }
+
+        return pascalsTriangele;
     }
 }
