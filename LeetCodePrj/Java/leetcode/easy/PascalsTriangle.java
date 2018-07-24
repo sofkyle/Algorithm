3f99b1cc245576d9dd3d24be606bb1f16498b080
glorychou <fightzyf1992@hotmail.com>
2018-07-24 23:13:12 +0800

    Pascal's Triangle

---
+++ LeetCodePrj/Java/leetcode/easy/PascalsTriangle.java
@@ -0,0 +1,13 @@
+package leetcode.easy;
+
+import java.util.List;
+
+/**
+ * @author Kyle
+ * @create 2018/7/24 23:12
+ */
+public class PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        return null;
+    }
+}
