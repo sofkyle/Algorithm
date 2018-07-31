e8f65dafeeaf5f0cdeb60435dd7bee3582bbd970
glorychou <fightzyf1992@hotmail.com>
2018-07-31 23:50:45 +0800

    try

--- LeetCodePrj/Java/leetcode/easy/PascalsTriangleII.java
+++ LeetCodePrj/Java/leetcode/easy/PascalsTriangleII.java
@@ -1,9 +1,13 @@
 package leetcode.easy;
 
+import java.util.List;
+
 /**
  * @author Kyle
  * @create 2018/7/26 23:19
  */
 public class PascalsTriangleII {
-
+    public List<Integer> getRow(int rowIndex) {
+        return null;
+    }
 }
