88400ee21c11ddd9177844fe7f7f4faae2368902
glorychou <fightzyf1992@hotmail.com>
2018-08-23 01:30:48 +0800

    LinkedList Cycle：AC

---
+++ LeetCodePrj/Java/leetcode/common/ListNode.java
@@ -0,0 +1,14 @@
+package leetcode.common;
+
+/**
+ * @author Kyle
+ * @create 2018/8/23 1:10
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
---
+++ LeetCodePrj/Java/leetcode/easy/LinkedListCycle.java
@@ -0,0 +1,28 @@
+package leetcode.easy;
+
+import leetcode.common.ListNode;
+
+/**
+ * @author Kyle
+ * @create 2018/8/23 1:08
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if(head == null) {
+            return false;
+        }
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast != null && slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
