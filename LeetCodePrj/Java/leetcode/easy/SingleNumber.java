7580537164b62da3b8713104727ce3b3011536b7
glorychou <fightzyf1992@hotmail.com>
2018-08-23 00:22:18 +0800

    Single Number: AC

--- LeetCodePrj/Java/leetcode/easy/SingleNumber.java
+++ LeetCodePrj/Java/leetcode/easy/SingleNumber.java
@@ -1,11 +1,30 @@
 package leetcode.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Kyle
  * @create 2018/8/22 0:46
  */
 public class SingleNumber {
     public int singleNumber(int[] nums) {
+        Map<Integer, Object> map = new HashMap<>();
+
+        for(int i : nums) {
+            if(map.containsKey(i)) {
+                map.put(i, 2);
+            } else {
+                map.put(i, 1);
+            }
+        }
+
+        for(Map.Entry entry : map.entrySet()) {
+            if(entry.getValue().equals(1)) {
+                return Integer.valueOf(entry.getKey().toString());
+            }
+        }
+
         return 0;
     }
 }
