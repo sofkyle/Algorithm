27a7dca2d12148b9c1d82655695cd57943d07930
glorychou <fightzyf1992@hotmail.com>
2018-08-28 23:35:21 +0800

    Intersection of Two Linked Lists: commit

---
+++ LeetCodePrj/Java/leetcode/easy/TwoSumII.java
@@ -0,0 +1,12 @@
+package leetcode.easy;
+
+/**
+ * @author Kyle
+ * @create 2018/8/28 0:21
+ */
+public class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+
+        return null;
+    }
+}
