3acf1e12acc47d90d8fb393db1cd6ae6e82c20bf
glorychou <fightzyf1992@hotmail.com>
2018-08-29 23:57:59 +0800

    Two Sum II: commit

--- LeetCodePrj/Java/leetcode/easy/TwoSumII.java
+++ LeetCodePrj/Java/leetcode/easy/TwoSumII.java
@@ -1,12 +1,23 @@
 package leetcode.easy;
 
+import java.util.Arrays;
+
 /**
  * @author Kyle
  * @create 2018/8/28 0:21
  */
 public class TwoSumII {
     public int[] twoSum(int[] numbers, int target) {
-
+        for(int i = 0; i < numbers.length - 1; i++) {
+            for(int j = i + 1; j < numbers.length; j++) {
+                if(numbers[i] + numbers[j] > target) {
+                    break;
+                }
+                if(numbers[i] + numbers[j] ==  target) {
+                    return new int[]{i + 1, j + 1};
+                }
+            }
+        }
         return null;
     }
 }
