3baf46f67254ef75e30b1f00146ca63b725d1985
glorychou <fightzyf1992@hotmail.com>
2018-08-31 01:31:08 +0800

    Excel Sheet Column Number: optimize

--- LeetCodePrj/Java/leetcode/easy/ExcelSheetColumnNumber.java
+++ LeetCodePrj/Java/leetcode/easy/ExcelSheetColumnNumber.java
@@ -5,23 +5,14 @@ package leetcode.easy;
  * @create 2018/8/31 0:54
  */
 public class ExcelSheetColumnNumber {
-    private static final int radius = 26;
-
     public int titleToNumber(String s) {
-        char[] chars = s.toCharArray();
+        int res = 0;
         int length = s.length();
-        int columnNumber = 0;
-
-        int exp = radius;
-        columnNumber += chars[length - 1] - 'A' + 1;
-
-        if(length <= 1) return columnNumber;
-
-        for (int i = s.length() - 2; i >= 0; i--) {
-            columnNumber += (chars[i] - 'A' + 1) * exp;
-            exp *= radius;
+        for(int i = 0; i < length; i++){
+            char c = s.charAt(i);
+            int temp= (c - 'A') + 1;
+            res = res *26 +temp;
         }
-
-        return columnNumber;
+        return res;
     }
 }
