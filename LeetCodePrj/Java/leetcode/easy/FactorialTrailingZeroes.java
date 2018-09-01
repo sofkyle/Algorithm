7919a6a9687476cd3d8e6ab0bd5783d6cb438dd1
glorychou <fightzyf1992@hotmail.com>
2018-09-01 11:55:25 +0800

    Factorial Trailing Zeros: AC

--- LeetCodePrj/Java/leetcode/easy/FactorialTrailingZeroes.java
+++ LeetCodePrj/Java/leetcode/easy/FactorialTrailingZeroes.java
@@ -5,25 +5,14 @@ package leetcode.easy;
  * @create 2018/8/31 1:32
  */
 public class FactorialTrailingZeroes {
+
     public int trailingZeroes(int n) {
-        int zerosNum = 0;
-
-        for(int i = 0; i <= n; i+=5) {
-            if(i != 0) {
-                zerosNum += getCountsOfFive(i);
-            }
-        }
-
-        return zerosNum;
-    }
-
-    private int getCountsOfFive(int i) {
         int count = 0;
-
-        while (i % 5 == 0) {
-            count++;
-            i /= 5;
+        while(n > 4){
+            n = n/5;
+            count = count + n;
         }
+
         return count;
     }
 }
