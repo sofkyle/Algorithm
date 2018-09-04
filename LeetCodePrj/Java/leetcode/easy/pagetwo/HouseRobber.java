238e57ef27e6b52e24bc2dd970627fe97aed6f60
glorychou <fightzyf1992@hotmail.com>
2018-09-04 23:58:18 +0800

    House Robber: commit

--- LeetCodePrj/Java/leetcode/easy/pagetwo/HouseRobber.java
+++ LeetCodePrj/Java/leetcode/easy/pagetwo/HouseRobber.java
@@ -6,6 +6,20 @@ package leetcode.easy.pagetwo;
  */
 public class HouseRobber {
     public int rob(int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+
+        int length = nums.length;
+
+        if(length == 1) {
+            return nums[0];
+        }
+
+        if(length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+
         return 0;
     }
 }
