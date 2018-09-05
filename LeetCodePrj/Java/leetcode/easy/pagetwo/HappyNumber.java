ea5577c7ee2d7e294163aa5d74a15d9c30754833
glorychou <fightzyf1992@hotmail.com>
2018-09-05 23:52:30 +0800

    Happy Number: commit

---
+++ LeetCodePrj/Java/leetcode/easy/pagetwo/HappyNumber.java
@@ -0,0 +1,33 @@
+package leetcode.easy.pagetwo;
+
+/**
+ * @author Kyle
+ * @create 2018/9/5 2:01
+ */
+public class HappyNumber {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = sumSquareOfDigits(slow);
+            fast = sumSquareOfDigits(fast);
+            fast = sumSquareOfDigits(fast);
+        } while (slow != fast && slow != 1);
+
+        if(slow == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private int sumSquareOfDigits(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
