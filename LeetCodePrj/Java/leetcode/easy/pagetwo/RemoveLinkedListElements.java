eb30f69ea93d79ec6d33a51c9b6f16394ef697c7
glorychou <fightzyf1992@hotmail.com>
2018-09-05 23:56:55 +0800

    Remove Linked List Elements: Add

---
+++ LeetCodePrj/Java/leetcode/easy/pagetwo/RemoveLinkedListElements.java
@@ -0,0 +1,14 @@
+package leetcode.easy.pagetwo;
+
+import leetcode.common.ListNode;
+
+/**
+ * @author Kyle
+ * @create 2018/9/5 23:56
+ */
+public class RemoveLinkedListElements {
+    public ListNode removeElements(ListNode head, int val) {
+
+        return null;
+    }
+}
