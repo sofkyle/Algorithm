d0ef93fa507a89863d1b42994d16da5a55d04b1d
glorychou <fightzyf1992@hotmail.com>
2018-10-05 13:00:24 +0800

    Delete Node in a Linked List: AC

---
+++ LeetCodePrj/Java/leetcode/easy/pagetwo/DeleteNodeInALinkedList.java
@@ -0,0 +1,14 @@
+package leetcode.easy.pagetwo;
+
+import leetcode.common.ListNode;
+
+/**
+ * @author Kyle
+ * @create 2018/10/5 12:45
+ */
+public class DeleteNodeInALinkedList {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
