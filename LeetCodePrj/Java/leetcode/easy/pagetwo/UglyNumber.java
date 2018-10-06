7c77d81cc5cddf5b53d44224f2194f990c88a48f
glorychou <fightzyf1992@hotmail.com>
2018-10-07 07:54:40 +0800

    Ugly Number: AC

---
+++ LeetCodePrj/Java/leetcode/easy/pagetwo/UglyNumber.java
@@ -0,0 +1,22 @@
+package leetcode.easy.pagetwo;
+
+/**
+ * @author Kyle
+ * @create 2018/10/6 22:51
+ */
+public class UglyNumber {
+    public boolean isUgly(int num) {
+        if (num == 0) return false;
+
+        while (num % 2 == 0) {
+            num >>= 1;
+        }
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        while (num % 5 == 0) {
+            num /=5;
+        }
+        return num == 1;
+    }
+}
