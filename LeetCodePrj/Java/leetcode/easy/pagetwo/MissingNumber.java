2c5273f5dff2ca17a2808e0a06b722ec76602ae7
glorychou <fightzyf1992@hotmail.com>
2018-10-07 10:21:20 +0800

    Missing Number: AC

---
+++ LeetCodePrj/Java/leetcode/easy/pagetwo/MissingNumber.java
@@ -0,0 +1,19 @@
+package leetcode.easy.pagetwo;
+
+/**
+ * @author Kyle
+ * @create 2018/10/7 10:15
+ */
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int len = nums.length;
+        int sum = (1 + len) * len / 2;
+
+        int actSum = 0;
+        for (int i : nums) {
+            actSum += i;
+        }
+
+        return sum - actSum;
+    }
+}
