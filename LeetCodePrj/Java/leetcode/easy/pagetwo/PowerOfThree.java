e405ac282e536cc8461998569a792cceceda4069
glorychou <fightzyf1992@hotmail.com>
2018-10-12 13:29:41 +0800

    Power of Three: AC

---
+++ LeetCodePrj/Java/leetcode/easy/pagetwo/PowerOfThree.java
@@ -0,0 +1,13 @@
+package leetcode.easy.pagetwo;
+
+public class PowerOfThree {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) return false;
+
+        while (n != 0) {
+            if (n % 3 != 0 && n != 1) return false;
+            n /= 3;
+        }
+        return true;
+    }
+}
