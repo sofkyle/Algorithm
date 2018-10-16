b5cc8c03fb5c81004e6127b31f0a479fcbae291f
glorychou <fightzyf1992@hotmail.com>
2018-10-16 23:44:07 +0800

    Intersection of Two Arrays II: AC

--- LeetCodePrj/Java/leetcode/easy/pagetwo/IntersectionOfTwoArraysII.java
+++ LeetCodePrj/Java/leetcode/easy/pagetwo/IntersectionOfTwoArraysII.java
@@ -1,7 +1,26 @@
 package leetcode.easy.pagetwo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class IntersectionOfTwoArraysII {
     public int[] intersect(int[] nums1, int[] nums2) {
-        return null;
+        if (nums1.length == 0 || nums2.length == 0) return new int[0];
+
+        List<Integer> list1 = new ArrayList<>();
+        for (int i : nums1) {
+            list1.add(i);
+        }
+
+        int last = 0;
+        for (int j : nums2) {
+            if (list1.contains(j)) {
+                nums1[last++] = j;
+                list1.remove(list1.indexOf(j));
+            }
+        }
+
+        return Arrays.copyOf(nums1, last);
     }
 }
