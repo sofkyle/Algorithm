7cdacb6ef51894db6894861f9568fece4337e8e4
glorychou <fightzyf1992@hotmail.com>
2018-12-22 22:22:41 +0800

    Third Maximum Number: Add Unit Test

--- LeetCodePrj/Java/leetcode/easy/page3/EasyPage3.java
+++ LeetCodePrj/Java/leetcode/easy/page3/EasyPage3.java
@@ -1,4 +1,12 @@
 package leetcode.easy.page3;
 
+import org.junit.jupiter.api.Test;
+
 public class EasyPage3 {
+    @Test
+    public void testThirdMaximumNumber() {
+        int[] intArray = new int[]{3, 2, 1};
+        ThirdMaximumNumber thirdMaximumNumber = new ThirdMaximumNumber();
+        System.out.println(thirdMaximumNumber.thirdMax(intArray));
+    }
 }
