d8f7f260a4b27a283d617f633e04481ec2a7c16b
glorychou <fightzyf1992@hotmail.com>
2018-12-24 10:28:09 +0800

    Construct Quad Tree: Add

---
+++ LeetCodePrj/Java/leetcode/easy/page3/ConstructQuadTree.java
@@ -0,0 +1,27 @@
+package leetcode.easy.page3;
+
+public class ConstructQuadTree {
+    public Node construct(int[][] grid) {
+        return null;
+    }
+
+    static class Node {
+        public boolean val;
+        public boolean isLeaf;
+        public Node topLeft;
+        public Node topRight;
+        public Node bottomLeft;
+        public Node bottomRight;
+
+        public Node() {}
+
+        public Node(boolean _val,boolean _isLeaf,Node _topLeft,Node _topRight,Node _bottomLeft,Node _bottomRight) {
+            val = _val;
+            isLeaf = _isLeaf;
+            topLeft = _topLeft;
+            topRight = _topRight;
+            bottomLeft = _bottomLeft;
+            bottomRight = _bottomRight;
+        }
+    };
+}
