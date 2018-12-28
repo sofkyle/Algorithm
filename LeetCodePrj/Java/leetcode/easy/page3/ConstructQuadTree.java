6221a82d5d9a83dd2e6292e3524087f4e5d599eb
glorychou <fightzyf1992@hotmail.com>
2018-12-28 22:45:47 +0800

    Construct Quad Tree: AC

--- LeetCodePrj/Java/leetcode/easy/page3/ConstructQuadTree.java
+++ LeetCodePrj/Java/leetcode/easy/page3/ConstructQuadTree.java
@@ -1,8 +1,36 @@
 package leetcode.easy.page3;
 
 public class ConstructQuadTree {
+
     public Node construct(int[][] grid) {
-        return null;
+        if (grid.length == 0) return null;
+
+        return recurse(grid, 0, 0, grid.length);
+    }
+
+    private Node recurse(int[][] grid, int x, int y, int length) {
+        if (length == 1) {
+            return new Node(grid[x][y] == 1, true,
+                    null, null, null, null);
+        }
+
+        length /= 2;
+
+        Node topLeftNode = recurse(grid, x, y, length);
+        Node topRightNode = recurse(grid, x, y + length, length);
+        Node bottomLeftNode = recurse(grid, x + length, y, length);
+        Node bottomRightNode = recurse(grid, x + length, y + length, length);
+
+        // Merging the leaves with same value
+        if (topLeftNode.isLeaf &&  topRightNode.isLeaf && topRightNode.isLeaf
+                && bottomLeftNode.isLeaf && bottomRightNode.isLeaf
+                && topLeftNode.val == topRightNode.val && topRightNode.val == bottomLeftNode.val
+                && bottomLeftNode.val == bottomRightNode.val) {
+            return new Node(topLeftNode.val, true,
+                   null, null, null, null);
+        }
+
+        return new Node(false, false, topLeftNode, topRightNode, bottomLeftNode, bottomRightNode);
     }
 
     static class Node {
