d02fffcfbbd1555e246b69be40c5e5d54d5ad80d
glorychou <fightzyf1992@hotmail.com>
2019-01-15 23:55:26 +0800

    Number of Segments in a String: Add

---
+++ LeetCodePrj/Java/leetcode/easy/page3/NumberOfSegmentsInAString.java
@@ -0,0 +1,23 @@
+package leetcode.easy.page3;
+
+public class NumberOfSegmentsInAString {
+    public int countSegments(String s) {
+        int numOfSegments = 0;
+
+        int i = 0;
+        boolean startCount = true;
+        while (i < s.length()) {
+            if (startCount && (s.charAt(i) >= 'a' && s.charAt(i) <= 'z'
+                    || s.charAt(i) >= 'A' && s.charAt(i) <= 'Z')) {
+                numOfSegments++;
+                startCount = false;
+            }
+            if (s.charAt(i) < 'a' || s.charAt(i) > 'z'
+                    && s.charAt(i) < 'A' || s.charAt(i) > 'Z') {
+                startCount = true;
+            }
+        }
+
+        return numOfSegments;
+    }
+}
