13b29c5976ea075c5bf22cae70b9bc5b29f65e02
glorychou <fightzyf1992@hotmail.com>
2019-02-10 17:38:58 +0800

    Path Sum III: Run

--- LeetCodePrj/Java/leetcode/easy/page3/PathSumIII.java
+++ LeetCodePrj/Java/leetcode/easy/page3/PathSumIII.java
@@ -4,6 +4,13 @@ import leetcode.common.TreeNode;
 
 public class PathSumIII {
     public int pathSum(TreeNode root, int sum) {
-        return 0;
+        return pathSum(root.left, sum) + pathSum(root.right, sum) + numOfPath(root, sum);
+    }
+
+    private int numOfPath(TreeNode node, int sum) {
+        if (node == null) return 0;
+        return node.val == sum ? 1 : 0 +
+                numOfPath(node.left, sum - node.val) +
+                numOfPath(node.right, sum - node.val);
     }
 }
