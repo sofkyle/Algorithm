d875d4489031e9cc11efa3ca308857b916fa4ed6
glorychou <fightzyf1992@hotmail.com>
2019-02-19 23:54:12 +0800

    Arranging Coins: Optimize AC

--- LeetCodePrj/Java/leetcode/easy/page3/ArrangingCoins.java
+++ LeetCodePrj/Java/leetcode/easy/page3/ArrangingCoins.java
@@ -2,9 +2,7 @@ package leetcode.easy.page3;
 
 public class ArrangingCoins {
 
-    private final int maxRow = 65537;
-
     public int arrangeCoins(int n) {
-        return (int) Math.sqrt(1 + 8 * n);
+        return (int) ((Math.sqrt(1 + 8l * n) - 1) / 2);
     }
 }
