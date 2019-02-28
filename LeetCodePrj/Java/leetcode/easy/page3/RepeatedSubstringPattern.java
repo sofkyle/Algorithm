959f67eecc1825105c9cb9b3813852e66f2b1db3
glorychou <fightzyf1992@hotmail.com>
2019-02-28 08:40:10 +0800

    Repeated Substring Pattern: AC

--- LeetCodePrj/Java/leetcode/easy/page3/RepeatedSubstringPattern.java
+++ LeetCodePrj/Java/leetcode/easy/page3/RepeatedSubstringPattern.java
@@ -1,5 +1,7 @@
 package leetcode.easy.page3;
 
 public class RepeatedSubstringPattern {
-
+    public boolean repeatedSubstringPattern(String s) {
+        return (s + s).substring(1, 2 * s.length() - 1).contains(s);
+    }
 }
