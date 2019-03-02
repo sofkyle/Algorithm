b20c483cf2d05202aa0e30d356c85234415a520e
glorychou <fightzyf1992@hotmail.com>
2019-03-02 23:53:24 +0800

    Island Perimeter: AC

--- LeetCodePrj/Java/leetcode/easy/page3/IslandPerimeter.java
+++ LeetCodePrj/Java/leetcode/easy/page3/IslandPerimeter.java
@@ -2,12 +2,13 @@ package leetcode.easy.page3;
 
 public class IslandPerimeter {
     public int islandPerimeter(int[][] grid) {
-        int len = grid.length;
+        int height = grid.length;
+        int weight = grid[0].length;
 
         int perimeter = 0;
 
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len; j++) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < weight; j++) {
                 if (grid[i][j] == 1) {
                     if (i - 1 < 0) {
                         perimeter++;
@@ -19,12 +20,12 @@ public class IslandPerimeter {
                     } else {
                         if (grid[i][j - 1] == 0) perimeter++;
                     }
-                    if (i + 1 >= len) {
+                    if (i + 1 >= height) {
                         perimeter++;
                     } else {
                         if (grid[i + 1][j] == 0) perimeter++;
                     }
-                    if (j + 1 >= len) {
+                    if (j + 1 >= weight) {
                         perimeter++;
                     } else {
                         if (grid[i][j + 1] == 0) perimeter++;
