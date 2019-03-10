9cb7a7ea126bb06dc59b32feaf7b3d51099ffee2
glorychou <fightzyf1992@hotmail.com>
2019-03-11 00:18:33 +0800

    Middle of the Linked List: AC

---
+++ LeetCodePrj/Java/leetcode/easy/page6/MiddleOfTheLinkedList.java
@@ -0,0 +1,17 @@
+package leetcode.easy.page6;
+
+import leetcode.common.ListNode;
+
+public class MiddleOfTheLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
