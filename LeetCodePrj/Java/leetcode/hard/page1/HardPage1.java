5a99e57095b7a3375c06fb0e74700ad110dfbb63
glorychou <fightzyf1992@hotmail.com>
2019-04-07 23:25:53 +0800

    Merge k Sorted Lists: Runtime Error

--- LeetCodePrj/Java/leetcode/hard/page1/HardPage1.java
+++ LeetCodePrj/Java/leetcode/hard/page1/HardPage1.java
@@ -1,5 +1,6 @@
 package leetcode.hard.page1;
 
+import leetcode.common.ListNode;
 import org.junit.jupiter.api.Test;
 
 public class HardPage1 {
@@ -28,5 +29,23 @@ public class HardPage1 {
         lruCache.put(4,1);
         lruCache.get(1);
         lruCache.get(2);
+    }
+
+    @Test
+    public void testMergeKSortedLists() {
+        ListNode node1 = new ListNode(1);
+        node1.next = new ListNode(4);
+        node1.next.next = new ListNode(5);
+
+        ListNode node2 = new ListNode(1);
+        node2.next = new ListNode(3);
+        node2.next.next = new ListNode(4);
+
+        ListNode node3 = new ListNode(2);
+        node3.next = new ListNode(6);
+
+        MergeKSortedLists mergeKSortedLists = new MergeKSortedLists();
+        ListNode[] lists = new ListNode[] {node1, node2, node3};
+        System.out.println(mergeKSortedLists.mergeKLists(lists));;
     }
 }
--- LeetCodePrj/Java/leetcode/hard/page1/MergeKSortedLists.java
+++ LeetCodePrj/Java/leetcode/hard/page1/MergeKSortedLists.java
@@ -4,6 +4,41 @@ import leetcode.common.ListNode;
 
 public class MergeKSortedLists {
     public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+        if (lists.length == 1) return lists[0];
 
+        ListNode sortedListHead = lists[0];
+
+        for (int i = 1; i < lists.length; i++) {
+            ListNode tmpFast = sortedListHead;
+            ListNode tmpSlow = sortedListHead;
+            ListNode cmpNode = lists[i];
+            while (tmpFast != null && cmpNode != null) {
+                if (tmpFast.val <= cmpNode.val) {
+                    if (tmpFast == tmpSlow) {
+                        tmpFast = tmpFast.next;
+                    } else {
+                        tmpFast = tmpFast.next;
+                        tmpSlow = tmpSlow.next;
+                    }
+                } else {
+                    ListNode cmpNodeNext = cmpNode.next;
+                    cmpNode.next = tmpFast;
+                    if (tmpFast == tmpSlow) {
+                        tmpSlow = cmpNode;
+                        sortedListHead = tmpSlow;
+                    } else {
+                        tmpSlow.next = cmpNode;
+                        tmpSlow = tmpSlow.next;
+                    }
+                    cmpNode = cmpNodeNext;
+                }
+            }
+            if (tmpFast == null) {
+                tmpSlow.next = cmpNode;
+            }
+        }
+
+        return sortedListHead;
     }
 }
