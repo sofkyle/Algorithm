0c50d29c0895a37730839ed2420ca30b2dbb4311
glorychou <fightzyf1992@hotmail.com>
2019-04-16 00:45:33 +0800

    Sort List: AC

---
+++ LeetCodePrj/Java/leetcode/medium/page2/MediumPage2.java
@@ -0,0 +1,20 @@
+package leetcode.medium.page2;
+
+import leetcode.common.ListNode;
+import leetcode.hard.page2.LFUCache;
+import org.junit.jupiter.api.Test;
+
+public class MediumPage2 {
+    @Test
+    public void testSortList() {
+        SortList sortList = new SortList();
+
+        ListNode node = new ListNode(4);
+        node.next = new ListNode(2);
+        node.next.next = new ListNode(1);
+        node.next.next.next = new ListNode(3);
+
+        ListNode sortedList = sortList.sortList(node);
+    }
+
+}
--- LeetCodePrj/Java/leetcode/medium/page2/SortList.java
+++ LeetCodePrj/Java/leetcode/medium/page2/SortList.java
@@ -1,7 +1,50 @@
 package leetcode.medium.page2;
 
+import leetcode.common.ListNode;
+
 public class SortList {
     public ListNode sortList(ListNode head) {
-        return null;
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode l2 = slow.next;
+        slow.next = null;
+
+        ListNode l1 = sortList(head);
+        l2 = sortList(l2);
+
+        return merge(l1, l2);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode mergeHead = new ListNode(0);
+        ListNode node = mergeHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                node.next = l1;
+                l1 = l1.next;
+            } else {
+                node.next = l2;
+                l2 = l2.next;
+            }
+            node = node.next;
+        }
+
+        if (l1 != null) {
+            node.next = l1;
+        } else {
+            node.next = l2;
+        }
+
+        return mergeHead.next;
     }
 }
