799a66af8545c94d07ae7766cf0286db8ca660c5
KyleChou <fightzyf1992@hotmail.com>
2019-05-18 10:40:06 +0800

    Heaters: AC

--- LeetCodePrj/Java/leetcode/easy/page3/Heaters.java
+++ LeetCodePrj/Java/leetcode/easy/page3/Heaters.java
@@ -2,6 +2,15 @@ package leetcode.easy.page3;
 
 public class Heaters {
     public int findRadius(int[] houses, int[] heaters) {
-        return 0;
+        int radius = 0;
+        for (int i = 0; i < houses.length; i++) {
+            int dis = Integer.MAX_VALUE;
+            for (int j = 0; j < heaters.length; j++) {
+                dis = Math.min(dis, Math.abs(houses[i] - heaters[j]));
+            }
+            radius = Math.max(radius, dis);
+        }
+
+        return radius;
     }
 }
