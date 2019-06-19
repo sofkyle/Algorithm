ce260685be5fcbc458ecfdd930d599ebb3cce79e
KyleChou <fightzyf1992@hotmail.com>
2019-06-19 23:56:03 +0800

    Maximum Gap: Thinking

--- LeetCodePrj/Java/leetcode/hard/page1/MaximumGap.java
+++ LeetCodePrj/Java/leetcode/hard/page1/MaximumGap.java
@@ -1,7 +1,25 @@
 package leetcode.hard.page1;
 
-public class MaximumGap {
-    public int maximumGap(int[] nums) {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class MaximumGap {
+
+    private boolean used = false;
+
+    public int maximumGap(int[] nums) {
+        if (nums.length < 2) {
+            return 0;
+        }
+
+        List<Integer> numList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        int mini = Collections.min(numList);
+        int maxi = Collections.max(numList);
+
+        int bucketSize = Math.max(1, (maxi - mini) / ((int) nums.length - 1));
+
+        return 0;
     }
 }
