000b24436d73f41f73f9ca06a0b877601fbba41c
KyleChou <fightzyf1992@hotmail.com>
2019-06-23 23:44:16 +0800

    Maximum Gap: finished

--- LeetCodePrj/Java/leetcode/hard/page1/MaximumGap.java
+++ LeetCodePrj/Java/leetcode/hard/page1/MaximumGap.java
@@ -1,13 +1,9 @@
 package leetcode.hard.page1;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class MaximumGap {
-
-    private boolean used = false;
 
     public int maximumGap(int[] nums) {
         if (nums.length < 2) {
@@ -19,7 +15,34 @@ public class MaximumGap {
         int maxi = Collections.max(numList);
 
         int bucketSize = Math.max(1, (maxi - mini) / ((int) nums.length - 1));
+        int bucketNum = (maxi - mini) / bucketSize + 1;
 
-        return 0;
+        Vector<Bucket> buckets = new Vector<>(bucketSize);
+
+        for (int num : nums) {
+            int bukcetIdx = (num - mini) / bucketSize;
+            Bucket bucket = new Bucket();
+            bucket.used = true;
+            bucket.minVal = Math.min(num, bucket.minVal);
+            bucket.maxVal = Math.max(num, bucket.maxVal);
+        }
+
+        int prevBuceketMax = mini, maxGap = 0;
+        for (Bucket bucket : buckets) {
+            if (!bucket.used) {
+                continue;
+            }
+
+            maxGap = Math.max(maxGap, bucket.minVal - prevBuceketMax);
+            prevBuceketMax = bucket.maxVal;
+        }
+
+        return maxGap;
+    }
+
+    class Bucket {
+        boolean used = false;
+        int minVal = Integer.MAX_VALUE;
+        int maxVal = Integer.MIN_VALUE;
     }
 }
