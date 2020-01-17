506735e8d92cb0efc6dbb4f22eef4791a2d96e7d
KyleChou <fightzyf1992@hotmail.com>
2020-01-17 11:51:13 +0800

    First Unique Character in a String: AC

--- LeetCodePrj/Java/leetcode/medium/page1/GroupAnagrams.java
+++ LeetCodePrj/Java/leetcode/medium/page1/GroupAnagrams.java
@@ -4,53 +4,20 @@ import java.util.*;
 
 public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<>(strs.length);
-        int count = 0;
+        List<List<String>> res = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
 
-        int[] flag = new int[strs.length];
+        int i = 0;
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
 
-        for (int i = 0; i < strs.length; i++) {
-            if (flag[i] == 1) {
-                continue;
+            String key = String.valueOf(chars);
+            if (!map.containsKey(key)) {
+                res.add(new ArrayList<>());
+                map.put(key, i++);
             }
-            List<String> curGroup = new LinkedList<>();
-            curGroup.add(strs[i]);
-
-            Map<Character, Integer> iFeature = new HashMap<>(strs[i].length());
-            for (int f = 0; f < strs[i].length(); f++) {
-                iFeature.put(strs[i].charAt(f), iFeature.getOrDefault(strs[i].charAt(f), 0) + 1);
-            }
-
-            for (int j = i + 1; j < strs.length; j++) {
-                if (strs[i].length() != strs[j].length()) {
-                    continue;
-                }
-
-                boolean sameGroup = true;
-                Map<Character, Integer> jFeature = new HashMap<>(strs[j].length());
-                for (int f = 0; f < strs[j].length(); f++) {
-                    jFeature.put(strs[j].charAt(f), jFeature.getOrDefault(strs[j].charAt(f), 0) + 1);
-                }
-
-                if (iFeature.size() != jFeature.size()) {
-                    sameGroup = false;
-                } else {
-                    for (Map.Entry entry : iFeature.entrySet()) {
-                        if (jFeature.get(entry.getKey()) != entry.getValue()) {
-                            sameGroup = false;
-                            break;
-                        }
-                    }
-                }
-
-                if (sameGroup) {
-                    curGroup.add(strs[j]);
-                    flag[j] = 1;
-                }
-            }
-
-            res.add(curGroup);
-            count++;
+            res.get(map.get(key)).add(s);
         }
 
         return res;
