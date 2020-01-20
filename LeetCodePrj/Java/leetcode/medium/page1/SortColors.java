44b9498a678fe7047b8298ec46b28b7d7d1d4607
KyleChou <fightzyf1992@hotmail.com>
2020-01-21 03:32:28 +0800

    Sort Colors: In-Place Optimize

--- LeetCodePrj/Java/leetcode/medium/page1/SortColors.java
+++ LeetCodePrj/Java/leetcode/medium/page1/SortColors.java
@@ -2,19 +2,17 @@ package leetcode.medium.page1;
 
 public class SortColors {
     public void sortColors(int[] nums) {
-        int[] tmp = new int[nums.length];
+        int p = 0;
 
-        int k = 0;
         for (int i = 0; i <= 2; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[j] == i) {
-                    tmp[k++] = i;
+            for (int q = p; q < nums.length; q++) {
+                if (nums[q] == i) {
+                    int tmp = nums[p];
+                    nums[p] = nums[q];
+                    nums[q] = tmp;
+                    p++;
                 }
             }
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = tmp[i];
         }
     }
 }
